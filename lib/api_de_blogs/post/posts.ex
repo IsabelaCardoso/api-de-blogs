ed8410d1e87b4e720a1fdef2e0a3d90974461146
isacardoso <isabela.cardoso@platosedu.com.br>
2022-03-27 19:29:10 -0300

    trata casos de erros - posts

--- lib/api_de_blogs/post/posts.ex
+++ lib/api_de_blogs/post/posts.ex
@@ -2,12 +2,23 @@ defmodule ApiDeBlogs.Post.Posts do
   alias ApiDeBlogs.{Repo, Post}
 
   def create(params, id) do
-    params
+    create_post = params
     |> Map.put("userId", id)
     |> Post.build()
-    |> create_post()
+
+
+    case create_post do
+      {:ok, post} -> {:ok, post}
+      {:error, %{errors: errors}} -> handle_error(errors)
+    end
   end
 
-  defp create_post({:ok, _changeset} = struct), do: struct
-  defp create_post({:error, _changeset} = error), do: error
+  defp handle_error([{_title, {"can't be blank", _}}] = errors) do
+    [{header, _id}] = errors
+    {:error, {header, :is_required}}
+  end
+
+  defp handle_error(errors) do
+    {:error, :bad_request}
+  end
 end
